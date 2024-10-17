f1342e70ea0b4fb650e09de93128a4e7fff2f445
Delion.SY <120167489+Grimoireheartz@users.noreply.github.com>
2024-10-17 16:29:40 +0700

    Add files via upload

--- Fontend/src/index.js
+++ Fontend/src/index.js
@@ -21,12 +21,60 @@ const App = () => {
     <div>
       <h1>SET Stock Data</h1>
       {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div>
+          {/* แสดงข้อมูลจากตาราง SET */}
+          {data.SET_data && (
+            <div>
+              <h2>SET Data</h2>
+              <pre>{JSON.stringify(data.SET_data, null, 2)}</pre>
+            </div>
+          )}
+
+          {/* แสดงข้อมูลจากตาราง mai */}
+          {data.mai_data && (
+            <div>
+              <h2>mai Data</h2>
+              <pre>{JSON.stringify(data.mai_data, null, 2)}</pre>
+            </div>
+          )}
+
+          {/* แสดงข้อมูลการขายชอร์ต */}
+          {data.overview_data && (
+            <div>
+              <h2>Short Selling Overview</h2>
+              <pre>{JSON.stringify(data.overview_data, null, 2)}</pre>
+            </div>
+          )}
+
+          {/* แสดงข้อมูลรายหลักทรัพย์ (Short Sell) */}
+          {data.short_sell_data && (
+            <div>
+              <h2>Short Sell Data (by Security)</h2>
+              <pre>{JSON.stringify(data.short_sell_data, null, 2)}</pre>
+            </div>
+          )}
+
+          {/* แสดงข้อมูลการทำธุรกรรมโปรแกรมเทรด */}
+          {data.program_trading_data && (
+            <div>
+              <h2>Program Trading Data</h2>
+              <pre>{JSON.stringify(data.program_trading_data, null, 2)}</pre>
+            </div>
+          )}
+
+          {/* แสดงข้อมูลรายหลักทรัพย์ (Program Trading) */}
+          {data.security_data && (
+            <div>
+              <h2>Security Data (Program Trading)</h2>
+              <pre>{JSON.stringify(data.security_data, null, 2)}</pre>
+            </div>
+          )}
+        </div>
       ) : (
         <p>Loading data...</p>
       )}
     </div>
   );
-}
+};
 
 export default App;
